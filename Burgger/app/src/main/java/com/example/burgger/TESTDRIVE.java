69f70e37ef6338d6d5f3a9f53643a7a6181dd504
ecyriaque <101125367+ecyriaque@users.noreply.github.com>
2023-03-29 15:39:41 +0200

    ajout de la possibilité d'enregistrer une photo

--- Burgger/app/src/main/java/com/example/burgger/TESTDRIVE.java
+++ Burgger/app/src/main/java/com/example/burgger/TESTDRIVE.java
@@ -1,16 +1,25 @@
 package com.example.burgger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.ImageView;
+import android.provider.OpenableColumns;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -20,94 +29,128 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.http.Multipart;
-import retrofit2.http.POST;
-import retrofit2.http.Part;
-
 
 public class TESTDRIVE extends AppCompatActivity {
-
-    private ImageView img;
+    private static final int REQUEST_PERMISSIONS_CODE = 1;
     private Uri mImageUri;
     private static final int PICK_IMAGE_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testdrive);
-        openFileChooser();
+
+        requestStoragePermissions();
     }
-    // Ouvrir la galerie pour sélectionner une photo
+
     private void openFileChooser() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
-    // Récupérer l'URI de l'image sélectionnée
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openFileChooser();
+            } else {
+                Toast.makeText(this, "Permissions are required to access files", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void requestStoragePermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS_CODE);
+            }
+        } else {
+            openFileChooser();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             mImageUri = data.getData();
-            uploadImage(); // Appeler la méthode pour envoyer la photo à l'API
+            uploadImage();
         }
     }
 
     private void uploadImage() {
-        // Créer une instance de Retrofit
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://burgerr7.000webhostapp.com/")
                 .build();
 
-        // Créer une instance de l'interface de l'API
         ApiInterface apiInterface = retrofit.create(ApiInterface.class);
 
-        // Obtenir le chemin réel de l'URI de l'image sélectionnée
-        String filePath = getRealPathFromURI(mImageUri);
-
-        // Créer une instance de la classe File à partir du chemin de l'image
-        File file = new File(filePath);
-
-        // Créer une instance de la classe RequestBody à partir du fichier
-        RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), file);
-
-        // Créer une instance de la classe MultipartBody.Part à partir de la RequestBody
-        MultipartBody.Part filePart = MultipartBody.Part.createFormData("image", file.getName(), requestBody);
-
-        // Envoyer la photo à l'API
-        Call<ResponseBody> call = apiInterface.uploadImage(filePart);
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    // La photo a été envoyée avec succès
-                } else {
-                    // Il y a eu une erreur lors de l'envoi de la photo
+        try (InputStream inputStream = getContentResolver().openInputStream(mImageUri)) {
+            if (inputStream != null) {
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                int nRead;
+                byte[] data = new byte[16384];
+                while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, nRead);
                 }
-            }
+                buffer.flush();
+                byte[] byteArray = buffer.toByteArray();
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                t.printStackTrace();
+                RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), byteArray);
+                MultipartBody.Part filePart = MultipartBody.Part.createFormData("image", getFileName(mImageUri), requestBody);
+
+                Call<ResponseBody> call = apiInterface.uploadImage(filePart);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(TESTDRIVE.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(TESTDRIVE.this, "Error uploading image", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(TESTDRIVE.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
-        });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public interface ApiInterface {
-        @Multipart
-        @POST("upload-picture.php")
-        Call<ResponseBody> uploadImage(@Part MultipartBody.Part image);
+        @retrofit2.http.Multipart
+        @retrofit2.http.POST("upload-picture.php")
+        Call<ResponseBody> uploadImage(@retrofit2.http.Part MultipartBody.Part image);
     }
-    private String getRealPathFromURI(Uri uri) {
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String filePath = cursor.getString(column_index);
-        cursor.close();
-        return filePath;
+
+    @SuppressLint("Range")
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                if (cursor == null) {
+                    cursor.close();
+                }
+            }
+        }        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 }
-
-
-
